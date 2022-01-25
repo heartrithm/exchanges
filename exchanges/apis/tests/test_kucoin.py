6fa4ac79ff57ab9966b78f098a9c689959f98046
edmond.tam <edmondtam2@gmail.com>
2022-01-25 15:17:15 +0000

    Update KuCoin to include auth

--- exchanges/apis/tests/test_kucoin.py
+++ exchanges/apis/tests/test_kucoin.py
@@ -9,7 +9,7 @@ from ..base import ExchangeApiException
 
 class KuCoinTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
-        self.client = exchange_factory("kucoin")("key", "secret")
+        self.client = exchange_factory("kucoin")("passphrase", "key", "secret")
         super(KuCoinTest, self).__init__(*args, **kwargs)
 
     def test_kucoin_status(self):
@@ -29,3 +29,9 @@ class KuCoinTest(unittest.TestCase):
             )
             result = self.client.brequest(1, "margin/trade/last", params={"currency": "USDT"})
             self.assertEqual(result, {"code": "200000", "data": [{"tradeId": "123", "currency": "USDT", "size": 100}]})
+
+    def test_auth(self):
+        with requests_mock.mock() as m:
+            m.delete("https://api.kucoin.com/api/v1/orders", json={"code": "200000", "data": {"cancelledOrderIds": []}})
+            result = self.client.brequest(1, "orders", True, "DELETE")
+            self.assertEqual(result, {"code": "200000", "data": {"cancelledOrderIds": []}})
