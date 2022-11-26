b894b637dc2bea0206dabc3e546ba1f9ea0438b0
Nick Sullivan <nick@sullivanflock.com>
2022-11-26 10:29:55 -0800

    Make setup executable

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import find_packages, setup
 
 setup(
     name="exchanges",
-    version="1.3.8",
-    description="Exchange APIs",
+    version="1.3.9",
+    description="Cryptocurrency Exchange APIs",
     packages=find_packages(),
     install_requires=["arrow", "cachetools", "loguru", "ratelimiter", "requests", "ujson"],
 )
